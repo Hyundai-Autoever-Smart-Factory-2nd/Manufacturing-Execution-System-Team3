28d2926e1b2e0596e0a1000f7d3cec6bdbdb7fc8
kimhotac <114737603+kimhotac@users.noreply.github.com>
2025-08-27 14:38:55 +0900

    refactor: Worksheet 엔티티와 DTO 분리

--- back/src/main/java/org/example/back/service/WorksheetService.java
+++ back/src/main/java/org/example/back/service/WorksheetService.java
@@ -1,5 +1,6 @@
 package org.example.back.service;
 
+import org.example.back.dto.CreateWorksheetRequest;
 import org.example.back.dto.WorksheetWithAllResponse;
 import org.example.back.mapper.WorksheetMapper;
 import org.example.back.model.Worksheet;
@@ -16,7 +17,15 @@ public class WorksheetService {
 
     public List<WorksheetWithAllResponse> findAll() { return worksheetMapper.findAll(); }
     public WorksheetWithAllResponse findByMachineId(Long id) { return worksheetMapper.findByMachineId(id); }
-    public void insertWorksheet(Worksheet worksheet) { worksheetMapper.insertWorksheet(worksheet); }
+    public void insertWorksheet(CreateWorksheetRequest request) {
+        Worksheet worksheet = new Worksheet();
+        worksheet.setAmount(request.getAmount());
+        worksheet.setWorkDate(request.getWorkDate());
+        worksheet.setMachineId(request.getMachineId());
+        worksheet.setProductId(request.getProductId());
+        worksheet.setWorkerId(request.getWorkerId());
+        worksheetMapper.insertWorksheet(worksheet);
+    }
     public void updateWorksheet(Worksheet worksheet) { worksheetMapper.updateWorksheet(worksheet); }
     public void deleteWorksheet(Long id) { worksheetMapper.deleteWorksheet(id); }
 }
