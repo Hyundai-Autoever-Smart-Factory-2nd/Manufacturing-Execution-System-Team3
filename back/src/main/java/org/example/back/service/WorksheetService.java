867ac1cd0f52776da2892a5999f5da5cbb421327
kimhotac <114737603+kimhotac@users.noreply.github.com>
2025-08-28 11:25:32 +0900

    feat: 작업자 화면 작업지시서 존재여부 검증로직

--- back/src/main/java/org/example/back/service/WorksheetService.java
+++ back/src/main/java/org/example/back/service/WorksheetService.java
@@ -16,7 +16,14 @@ public class WorksheetService {
     WorksheetMapper worksheetMapper;
 
     public List<WorksheetWithAllResponse> findAll() { return worksheetMapper.findAll(); }
-    public WorksheetWithAllResponse findByMachineId(Long id) { return worksheetMapper.findByMachineId(id); }
+    public WorksheetWithAllResponse findByMachineId(Long id) {
+        WorksheetWithAllResponse response = worksheetMapper.findByMachineId(id);
+        if (response == null) {
+            throw new IllegalArgumentException("해당 설비의 금일 작업지시서가 존재하지 않습니다.");
+        }
+        return response;
+    }
+
     public void insertWorksheet(CreateWorksheetRequest request) {
         if (worksheetMapper.existsByWorkDateAndMachineId(request)) {
             throw new IllegalArgumentException(request.getWorkDate() + " 에 해당 장비는 이미 작업지시서가 존재합니다.");
